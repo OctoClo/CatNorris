cd236566a6454a8bc9800e613bb2a71816c3369b
OctoClo <octoclo@zoho.com>
2017-02-24 12:30:55 +0100

    V1 : Blague de chuck (+ versions futures en brouillon)

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,28 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+// Initialiser l'application : ajout des listeners
+function initialize() {
+  chercherCitation();
+}
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+// Lancer l'application
+initialize();
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// Afficher la deuxième page
+function chercherCitation() {
+	// Requête HTTP pour récupérer la météo
+	var url = "http://api.icndb.com/jokes/random";
+	var requete = new XMLHttpRequest();
+	requete.open('GET', url, true);
+	requete.send();
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+	// Créer l'élément qui affiche la météo
+	requete.onreadystatechange = function (aEvt) {
+		if (requete.readyState == 4) {
+			if (requete.status == 200) {
+				var reponse = requete.responseText;
+				var blague = JSON.parse(reponse);
 
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+				document.getElementById('citation').innerHTML = blague.value.joke;
+			}
+		}
+	}
+}
