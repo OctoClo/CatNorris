cfae8fd2d5d3d22d70474a431acf954669ed8e74
OctoClo <octoclo@zoho.com>
2017-03-10 12:28:16 +0100

    Version 3 : Citation inspirante et gif de chat + icone de l application

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,46 +1,141 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+// Lancer l'application
+initialize();
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+// Initialiser l'application : afficher une citation et un gif au hasard et mettre des listeners sur les boutons
+function initialize() {
+  chercherCitation("chuck");
+	chercherGif("chuck");
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	document.getElementById('boutonChuckChat').addEventListener('click', allerPageChat);
+  document.getElementById('boutonChatChuck').addEventListener('click', allerPageChuck);
+  //document.getElementById('boutonActualiser').addEventListener('click', actualiser);
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+// Afficher la page des chats
+function allerPageChat() {
+	// Cacher page Chuck et afficher page chat
+	document.getElementById('contenuPageChuck').style.display = "none";
+	document.getElementById('contenuPageChat').style.display = "block";
 
-        console.log('Received Event: ' + id);
-    }
-};
+	// Afficher une citation et un gif au hasard
+	chercherCitation("chat");
+	chercherGif("chat");
+}
 
-app.initialize();
+// Afficher la page de Chuck
+function allerPageChuck() {
+	// Cacher page chat et afficher page Chuck
+	document.getElementById('contenuPageChat').style.display = "none";
+	document.getElementById('contenuPageChuck').style.display = "block";
+
+	// Afficher une citation et un gif au hasard
+	chercherCitation("chuck");
+	chercherGif("chuck");
+}
+
+// Récupérer une citation
+function chercherCitation(type) {
+	// Requête HTTP pour récupérer la citation
+	if (type == "chuck")
+		var url = "http://api.icndb.com/jokes/random";
+	else if (type == "chat")
+		//var url = "http://api.forismatic.com/api/1.0/method=getQuote&format=json&language=en";
+		var url = "http://quotes.rest/qod.json?category=inspire";
+
+	var requete = new XMLHttpRequest();
+
+	// Envoi de la requête
+	requete.open('GET', url, true);
+	requete.send();
+
+	// Modifier le texte pour afficher la citation quand la requête revient
+	requete.onreadystatechange = function (aEvt) {
+		if (requete.readyState == 4) {
+			if (requete.status == 200) {
+				var reponse = requete.responseText;
+				var citation = JSON.parse(reponse);
+
+				if (type == "chuck")
+					document.getElementById('citationChuck').innerHTML = citation.value.joke;
+				else if (type == "chat")
+					document.getElementById('citationChat').innerHTML = citation.contents.quotes[0].quote;
+			}
+		}
+	}
+}
+
+// Récupérer un gif
+function chercherGif(type) {
+	// Requête HTTP pour récupérer le gif
+	var apiKey = "dc6zaTOxFJmzC";
+	var id = idGifAleatoire(type);
+	var url = "http://api.giphy.com/v1/gifs/" + id + "?api_key=" + apiKey;
+	var requete = new XMLHttpRequest();
+
+	// Envoi de la requête
+	requete.open('GET', url, true);
+	requete.send();
+
+	// Modifier l'image pour afficher l'image quand la requête revient
+	requete.onreadystatechange = function (aEvt) {
+		if (requete.readyState == 4) {
+			if (requete.status == 200) {
+				var reponse = requete.responseText;
+				var gif = JSON.parse(reponse);
+
+				if (type == "chuck")
+					document.getElementById('gifChuck').src = gif.data.images.fixed_height.url;
+				else if (type == "chat")
+					document.getElementById('gifChat').src = gif.data.images.fixed_height.url;
+			}
+		}
+	}
+}
+
+// Retourne un id de gif aléatoire
+function idGifAleatoire(type) {
+	if (type == "chuck") {
+		// Tableau des id de gifs de Chuck Norris
+		var idGifChuck = [
+			"RMQ7kUUhfcYj6",
+			"7qZ3ZX1Gu3TZm",
+			"wQvFuXLMOtw8o",
+			"ylp4hl9xEaWyc",
+			"VDX3bYZTc4ROg",
+			"cY0bN5cDAzyjm",
+			"uUfbtweIW3A7S",
+			"WILokrHw5V8By",
+			"PcVGf1Wg7peh2",
+			"Bid9Pf0d4re6c",
+			"ZWyNa2Lll2tBS",
+			"UUERCo8UPauDS"
+		];
+
+		// Génère un index aléatoire entre 0 et la taille du tableau des id
+		var index = Math.floor(Math.random() * idGifChuck.length);
+
+		// Retourne l'id à l'index généré
+		return idGifChuck[index];
+	}
+	else if (type == "chat") {
+		// Tableau des id de gifs de chats
+		var idGifChat = [
+			"13CoXDiaCcCoyk",
+			"JIX9t2j0ZTN9S",
+			"yFQ0ywscgobJK",
+			"Alfb46lOc1kVG",
+			"uTCAwWNtz7U2c",
+			"ND6xkVPaj8tHO",
+			"8u9PS5l5znIbe",
+			"TKfywHrPHpJiE",
+			"17Q92poP1qJwI",
+			"17Q92poP1qJwI"
+		];
+
+		// Génère un index aléatoire entre 0 et la taille du tableau des id
+		var index = Math.floor(Math.random() * idGifChat.length);
+
+		// Retourne l'id à l'index généré
+		return idGifChat[index];
+	}
+}
--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,46 @@
 // Lancer l'application
 initialize();
 
-// Initialiser l'application : afficher une citation et un gif au hasard
+// Initialiser l'application : afficher une citation et un gif au hasard et mettre des listeners sur les boutons
 function initialize() {
-  chercherCitation();
-	chercherGif();
+  chercherCitation("chuck");
+	chercherGif("chuck");
+
+	document.getElementById('boutonChuckChat').addEventListener('click', allerPageChat);
+  document.getElementById('boutonChatChuck').addEventListener('click', allerPageChuck);
+  //document.getElementById('boutonActualiser').addEventListener('click', actualiser);
+}
+
+// Afficher la page des chats
+function allerPageChat() {
+	// Cacher page Chuck et afficher page chat
+	document.getElementById('contenuPageChuck').style.display = "none";
+	document.getElementById('contenuPageChat').style.display = "block";
+
+	// Afficher une citation et un gif au hasard
+	chercherCitation("chat");
+	chercherGif("chat");
+}
+
+// Afficher la page de Chuck
+function allerPageChuck() {
+	// Cacher page chat et afficher page Chuck
+	document.getElementById('contenuPageChat').style.display = "none";
+	document.getElementById('contenuPageChuck').style.display = "block";
+
+	// Afficher une citation et un gif au hasard
+	chercherCitation("chuck");
+	chercherGif("chuck");
 }
 
 // Récupérer une citation
-function chercherCitation() {
+function chercherCitation(type) {
 	// Requête HTTP pour récupérer la citation
-	var url = "http://api.icndb.com/jokes/random";
+	if (type == "chuck")
+		var url = "http://api.icndb.com/jokes/random";
+	else if (type == "chat")
+		var url = "http://quotes.rest/qod.json?category=inspire";
+
 	var requete = new XMLHttpRequest();
 
 	// Envoi de la requête
@@ -22,19 +52,22 @@ function chercherCitation() {
 		if (requete.readyState == 4) {
 			if (requete.status == 200) {
 				var reponse = requete.responseText;
-				var blague = JSON.parse(reponse);
+				var citation = JSON.parse(reponse);
 
-				document.getElementById('citation').innerHTML = blague.value.joke;
+				if (type == "chuck")
+					document.getElementById('citationChuck').innerHTML = citation.value.joke;
+				else if (type == "chat")
+					document.getElementById('citationChat').innerHTML = citation.contents.quotes[0].quote;
 			}
 		}
 	}
 }
 
 // Récupérer un gif
-function chercherGif() {
+function chercherGif(type) {
 	// Requête HTTP pour récupérer le gif
 	var apiKey = "dc6zaTOxFJmzC";
-	var id = idGifAleatoire();
+	var id = idGifAleatoire(type);
 	var url = "http://api.giphy.com/v1/gifs/" + id + "?api_key=" + apiKey;
 	var requete = new XMLHttpRequest();
 
@@ -49,33 +82,61 @@ function chercherGif() {
 				var reponse = requete.responseText;
 				var gif = JSON.parse(reponse);
 
-				document.getElementById('gif').src = gif.data.images.fixed_height.url;
+				if (type == "chuck")
+					document.getElementById('gifChuck').src = gif.data.images.fixed_height.url;
+				else if (type == "chat")
+					document.getElementById('gifChat').src = gif.data.images.fixed_height.url;
 			}
 		}
 	}
 }
 
 // Retourne un id de gif aléatoire
-function idGifAleatoire() {
-	// Tableau des id de gifs de Chuck Norris
-	var idGifChuck = [
-		"RMQ7kUUhfcYj6",
-		"7qZ3ZX1Gu3TZm",
-		"wQvFuXLMOtw8o",
-		"ylp4hl9xEaWyc",
-		"VDX3bYZTc4ROg",
-		"cY0bN5cDAzyjm",
-		"uUfbtweIW3A7S",
-		"WILokrHw5V8By",
-		"PcVGf1Wg7peh2",
-		"Bid9Pf0d4re6c",
-		"ZWyNa2Lll2tBS",
-		"UUERCo8UPauDS"
-	];
+function idGifAleatoire(type) {
+	if (type == "chuck") {
+		// Tableau des id de gifs de Chuck Norris
+		var idGifChuck = [
+			"RMQ7kUUhfcYj6",
+			"7qZ3ZX1Gu3TZm",
+			"wQvFuXLMOtw8o",
+			"ylp4hl9xEaWyc",
+			"VDX3bYZTc4ROg",
+			"cY0bN5cDAzyjm",
+			"uUfbtweIW3A7S",
+			"WILokrHw5V8By",
+			"PcVGf1Wg7peh2",
+			"Bid9Pf0d4re6c",
+			"ZWyNa2Lll2tBS",
+			"UUERCo8UPauDS"
+		];
 
-	// Génère un index aléatoire entre 0 et la taille du tableau des id
-	var index = Math.floor(Math.random() * idGifChuck.length);
+		// Génère un index aléatoire entre 0 et la taille du tableau des id
+		var index = Math.floor(Math.random() * idGifChuck.length);
 
-	// Retourne l'id à l'index généré
-	return idGifChuck[index];
+		// Retourne l'id à l'index généré
+		return idGifChuck[index];
+	}
+	else if (type == "chat") {
+		// Tableau des id de gifs de chats
+		var idGifChat = [
+			"13CoXDiaCcCoyk",
+			"JIX9t2j0ZTN9S",
+			"yFQ0ywscgobJK",
+			"Alfb46lOc1kVG",
+			"uTCAwWNtz7U2c",
+			"ND6xkVPaj8tHO",
+			"8u9PS5l5znIbe",
+			"TKfywHrPHpJiE",
+			"17Q92poP1qJwI",
+			"INUsrrxQW4et2",
+			"SgZtvjwcfq0ww",
+			"OmK8lulOMQ9XO"
+		];
+
+		// Génère un index aléatoire entre 0 et la taille du tableau des id
+		var index = Math.floor(Math.random() * idGifChat.length);
+
+		// Retourne l'id à l'index généré
+		return idGifChat[index];
+	}
 }
