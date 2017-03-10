beea5c7d98ac47ab83b2828e04337aaa5ba4b8e4
OctoClo <octoclo@zoho.com>
2017-03-10 10:49:03 +0100

    V2 : Gif de Chuck aleatoire parmi une liste d id

--- www/js/index.js
+++ www/js/index.js
@@ -1,20 +1,23 @@
-// Initialiser l'application : ajout des listeners
-function initialize() {
-  chercherCitation();
-}
-
 // Lancer l'application
 initialize();
 
-// Afficher la deuxième page
+// Initialiser l'application : afficher une citation et un gif au hasard
+function initialize() {
+  chercherCitation();
+	chercherGif();
+}
+
+// Récupérer une citation
 function chercherCitation() {
-	// Requête HTTP pour récupérer la météo
+	// Requête HTTP pour récupérer la citation
 	var url = "http://api.icndb.com/jokes/random";
 	var requete = new XMLHttpRequest();
+
+	// Envoi de la requête
 	requete.open('GET', url, true);
 	requete.send();
 
-	// Créer l'élément qui affiche la météo
+	// Modifier le texte pour afficher la citation quand la requête revient
 	requete.onreadystatechange = function (aEvt) {
 		if (requete.readyState == 4) {
 			if (requete.status == 200) {
@@ -25,4 +28,54 @@ function chercherCitation() {
 			}
 		}
 	}
+}
+
+// Récupérer un gif
+function chercherGif() {
+	// Requête HTTP pour récupérer le gif
+	var apiKey = "dc6zaTOxFJmzC";
+	var id = idGifAleatoire();
+	var url = "http://api.giphy.com/v1/gifs/" + id + "?api_key=" + apiKey;
+	var requete = new XMLHttpRequest();
+
+	// Envoi de la requête
+	requete.open('GET', url, true);
+	requete.send();
+
+	// Modifier l'image pour afficher l'image quand la requête revient
+	requete.onreadystatechange = function (aEvt) {
+		if (requete.readyState == 4) {
+			if (requete.status == 200) {
+				var reponse = requete.responseText;
+				var gif = JSON.parse(reponse);
+
+				document.getElementById('gif').src = gif.data.images.fixed_height.url;
+			}
+		}
+	}
+}
+
+// Retourne un id de gif aléatoire
+function idGifAleatoire() {
+	// Tableau des id de gifs de Chuck Norris
+	var idGifChuck = [
+		"RMQ7kUUhfcYj6",
+		"7qZ3ZX1Gu3TZm",
+		"wQvFuXLMOtw8o",
+		"ylp4hl9xEaWyc",
+		"VDX3bYZTc4ROg",
+		"cY0bN5cDAzyjm",
+		"uUfbtweIW3A7S",
+		"WILokrHw5V8By",
+		"PcVGf1Wg7peh2",
+		"Bid9Pf0d4re6c",
+		"ZWyNa2Lll2tBS",
+		"UUERCo8UPauDS"
+	];
+
+	// Génère un index aléatoire entre 0 et la taille du tableau des id
+	var index = Math.floor(Math.random() * idGifChuck.length);
+
+	// Retourne l'id à l'index généré
+	return idGifChuck[index];
 }
